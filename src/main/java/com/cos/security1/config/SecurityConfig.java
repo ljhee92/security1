ead7a6959ead9ba7ccb1120961df79914666ef6d
ljhee92 <weilainib7@gmail.com>
2024-12-23 16:23:08 +0900

    feat: 시큐리티 권한 설정

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.cos.security1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터(SecurityConfig)가 스프링 필터체인에 등록됨
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true) // Secure 어노테이션 활성화, PreAuthorize, PostAuthorize 어노테이션 활성화
 public class SecurityConfig {
     // @Bean : 해당 메서드의 리턴되는 객체를 IoC로 등록
     @Bean
@@ -25,9 +28,9 @@ public class SecurityConfig {
                         .requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN") // 특정 권한 필요
                         .requestMatchers("/admin/**").hasRole("ADMIN") // 관리자 권한 필요
                         .anyRequest().permitAll() // 그 외 권한 허용
-                );
+                )
 
-        http.formLogin(form -> form
+                .formLogin(form -> form
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // login 주소가 호출 -> 시큐리티가 낚아채서 로그인 대신 진행
                 .defaultSuccessUrl("/"));
--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.cos.security1.controller;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -53,7 +55,7 @@ public class IndexController {
     @PostMapping("/join")
     public String join(User user) {
         System.out.println(user);
-        user.setRole("USER");
+        user.setRole("ROLE_ USER");
 
         String rawPassword = user.getPassword();
         String encPassword = bCryptPasswordEncoder.encode(rawPassword);
@@ -61,5 +63,17 @@ public class IndexController {
 
         userRepository.save(user); // 회원가입은 잘 되나 패스워드 암호화가 되지 않아 시큐리티 로그인 불가
         return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/data")
+    public @ResponseBody String data() {
+        return "데이터정보";
     }
 }
